e49cc22d8cb993aed5781ac6d4df58e3cc548319
Peter Fänge <pellefange@gmail.com>
2025-02-25 18:35:13 +0100

    added function to count characters without spaces

--- stringUtils.ts
+++ stringUtils.ts
@@ -6,6 +6,10 @@ export const countCharacters = (str: string): number => {
     return str.length;
 };
 
+export const countCharsNoSpace = (str: string): number => {
+    return str.split("").length;
+}
+
 export const capitalizeFirstLetter = (str: string): string => {
     const firstLetter = str.slice(0, 1);
     return firstLetter.toUpperCase() + str.substring(1);
