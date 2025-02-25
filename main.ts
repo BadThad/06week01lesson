6c6ba2152581ff17c3c1b7b23f4860e41d38747c
Peter Fänge <pellefange@gmail.com>
2025-02-25 18:51:30 +0100

    added functions to write file and log directory path

--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
-import { reverseString, countCharacters, capitalizeFirstLetter } from "./stringUtils";
+import { reverseString, countCharacters, capitalizeFirstLetter, countCharsNoSpace } from "./stringUtils";
+import fs from "fs";
+import path from "path";
+
+const sentence = "do you know what a palindrome is? It's a word that is spelled the same way backwards. Such as kayak, racecar, or deified."
+
+const reverseSentence = reverseString(sentence);
+fs.writeFileSync("reverseSentence.txt", reverseSentence);
+
+const filePath = path.join(__dirname, 'reverseSentence.txt');
+console.log('New text file for reversed sentence save at: ' + filePath);
+
+console.log(countCharsNoSpace(sentence));
 
 console.log(reverseString("sdrawkcab"));
 
-console.log(countCharacters("sdrawkcab"));
+console.log(countCharacters(sentence));
 
 console.log(capitalizeFirstLetter("sdrawkcab"));
